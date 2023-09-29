a9a768c284e77d3a72455aaf75f862aab5af157f
Taurusilver1107 <141950465+Taurusilver1107@users.noreply.github.com>
2023-09-29 09:33:12 +0000

    Authentification

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,10 +1,9 @@
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div>
-      <p>
-        This is protected page
-      </p>
+      <UserButton afterSignOutUrl="/" />
     </div>
     
   )
